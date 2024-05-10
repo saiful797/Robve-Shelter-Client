91df8c6519e566121780a5f965ac948b1d1296aa
saiful797 <saifulislamsaif797@gmail.com>
2024-05-10 12:13:23 +0600

    Updated navbar section

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        Madimi: ["Madimi One", 'sans-serif'],
+      }
+    },
   },
   plugins: [
     require('daisyui'),
