c43bbb0e076f0815af53fb2adc924b5eb1ceef15
saiful797 <saifulislamsaif797@gmail.com>
2024-05-11 20:11:19 +0600

    Updated featured rooms section

--- src/Pages/HomePage/featuredRooms/Room/Room.jsx
+++ src/Pages/HomePage/featuredRooms/Room/Room.jsx
@@ -1,14 +1,25 @@
 import PropTypes from 'prop-types';
+import './Room.css'
 
 const Room = ({room}) => {
-    const {image, description} = room;
+    const {image, room_id, room_size, availability, price, description} = room;
     return (
         <div className="card w-[350px] h-[650px] bg-blue-950 mx-auto">
-            <figure><img className='h-72 w-full' src={image} alt="room image"/></figure>
-            <div className="card-body">
-                <p className='text-white'>{description}</p>
-                <div className="card-actions justify-end">
-                    <button className="btn btn-primary">Book Now</button>
+            <figure><img className='h-72 w-full rounded-b-[60px] border-b-4 border-orange-500' src={image} alt="room image"/></figure>
+            <div className="card-body text-white relative">
+                {/* <p className='text-white'>{description}</p> */}
+                <h1 className='text-xl font-extralight'>Room ID: <span className='text-xl font-medium'>{room_id}</span></h1>
+                <h2 className='text-xl font-extralight'>Room Size: <span className='text-2xl font-medium'>{room_size}</span></h2>
+                {
+                    availability === true? <h3 className="text-xl font-extralight">Room Availability
+                    : <span className="text-2xl font-bold text-green-600">Available</span></h3>
+                    :
+                    <h3 className="text-xl font-extralight">Room Availability: <span className="text-2xl font-bold text-red-600">Unavailable</span></h3>
+                }
+                <h2  className="text-xl font-extralight">Price(Per Night): <span className='text-2xl font-medium'>${price-1000}</span></h2>
+                <h3 className='truncate-3-lines text-justify'><span className="text-2xl font-bold text-stone-400">Description: </span>  {description}</h3>
+                <div className="card-actions justify-end absolute bottom-2 w-4/5">
+                    <button className="btn btn-ghost bg-gradient-to-r from-pink-500 to-yellow-500 rounded-none border-none w-full hover:from-black hover:to-black text-xl font-medium">Book Now</button>
                 </div>
             </div>
         </div>
