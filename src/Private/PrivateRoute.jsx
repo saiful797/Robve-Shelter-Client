ff65771c89915615a6fe2fee191e1f5196b93012
saiful797 <saifulislamsaif797@gmail.com>
2024-05-11 22:48:12 +0600

    Added private route

---
+++ src/Private/PrivateRoute.jsx
@@ -0,0 +1,11 @@
+import PropTypes from 'prop-types';
+
+const PrivateRoute = ({children}) => {
+    
+    return children;
+};
+
+PrivateRoute.propTypes ={
+    children: PropTypes.node,
+}
+export default PrivateRoute;
