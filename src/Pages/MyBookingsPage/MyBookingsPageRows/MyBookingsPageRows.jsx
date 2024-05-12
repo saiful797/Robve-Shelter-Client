73397ccf8005719d967f93b32fd119dc1c7c6239
saiful797 <saifulislamsaif797@gmail.com>
2024-05-13 00:17:53 +0600

    Updated Row information

--- src/Pages/MyBookingsPage/MyBookingsPageRows/MyBookingsPageRows.jsx
+++ src/Pages/MyBookingsPage/MyBookingsPageRows/MyBookingsPageRows.jsx
@@ -5,7 +5,7 @@ const MyBookingsPageRows = ({booking, indx}) => {
     const {room_id, price, image, offers} = booking;
     return (
         <tr>
-            <th className="border-2">
+            <th className="border-2 text-lg">
                 <h1>{indx+1}</h1>
             </th>
             <td className="border-2">
@@ -17,16 +17,16 @@ const MyBookingsPageRows = ({booking, indx}) => {
                     </div>
                 </div>
             </td>
-            <td className="border-2">
+            <td className="border-2 text-lg">
                 {room_id}
             </td>
-            <td className="border-2">
-               {price-1000}
+            <td className="border-2 text-lg">
+               ${price-1000}
             </td>
-            <td className="border-2">
+            <td className="border-2 text-lg">
                 {offers}
             </td>
-            <th className="border-2">
+            <th className="border-2 text-lg">
                 <div>
                     <button className="btn btn-ghost btn-xs">details</button>
                     <button className="btn btn-ghost btn-xs">details</button>
