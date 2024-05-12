3de84de3157a0f18b8bee040339e063da3d6ed8f
saiful797 <saifulislamsaif797@gmail.com>
2024-05-12 14:11:44 +0600

    updated Booking Room Modal and handle error successfully

--- src/Pages/BookingRoomModal/BookingRoomModal.jsx
+++ src/Pages/BookingRoomModal/BookingRoomModal.jsx
@@ -1,8 +1,9 @@
 import PropTypes from 'prop-types';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import useURL from '../../Hooks/useURL/useURL';
 import axios from 'axios';
 import useAuth from '../../Hooks/useAuth/useAuth';
+import { useForm } from 'react-hook-form';
 
 const BookingRoomModal = ({id}) => {
     const url = useURL();
@@ -10,11 +11,21 @@ const BookingRoomModal = ({id}) => {
     const {user} = useAuth();
     const [bookedRoom, SetBookedRoom] = useState({});
     
-    axios.get(`${url}/specificRoom/${id}`)
-        .then(res => {
-        SetBookedRoom(res.data);
-        // console.log(res.data);
-    })
+    useEffect(() => {
+        axios.get(`${url}/specificRoom/${id}`)
+         .then(res => {
+            SetBookedRoom(res.data);
+         })
+
+    },[url, id]);
+
+    const {register ,reset , handleSubmit} = useForm();
+    const onSubmit = (data) => {
+        
+        console.log(data);
+        reset();
+    }
+
     
     const {room_id, description, price, room_size, offers} = bookedRoom;
     return (
@@ -28,7 +39,7 @@ const BookingRoomModal = ({id}) => {
             </div>
             <div className='border-8 mt-5 p-3 rounded-3xl'>
                 <h1 className='text-center text-3xl font-medium text-orange-500'>Book Your Room</h1>
-                <form className='mt-10'>
+                <form onSubmit={handleSubmit(onSubmit)} className='mt-10'>
                     <div className='space-y-3'>
                         <h2 className='text-2xl font-medium'>Your Name: <span className='text-amber-500'>{user.displayName}</span></h2>
                         <p className='text-2xl font-medium'>Email: <span className='text-amber-500'>{user.email}</span></p>
@@ -37,9 +48,9 @@ const BookingRoomModal = ({id}) => {
                         <label className="label">
                             <span className="text-xl text-white">Choose Booking Date:</span>
                         </label>
-                        <input type="date" name='date' className="input input-bordered" required />
+                        <input type="date" name='date' className="input input-bordered bg-zinc-400" {...register("date")} required />
                     </div>
-                    <input className='btn btn-outline btn-info mt-5 w-full' type="submit" value="CONFIRM BOOKING" />
+                    <input className='btn btn-outline bg-black text-white hover:bg-black mt-5 w-full' type="submit" value="CONFIRM BOOKING" />
                 </form>
             </div>
         </div>
