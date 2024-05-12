09c47c85bf294af36fba5e81575c17f72b56c14c
saiful797 <saifulislamsaif797@gmail.com>
2024-05-12 09:59:30 +0600

    Get specific room id , when click book now button

--- src/Pages/HomePage/featuredRooms/Room/Room.jsx
+++ src/Pages/HomePage/featuredRooms/Room/Room.jsx
@@ -2,7 +2,12 @@ import PropTypes from 'prop-types';
 import './Room.css'
 
 const Room = ({room}) => {
-    const {image, room_id, room_size, availability, price, description} = room;
+    const {_id, image, room_id, room_size, availability, price, description} = room;
+
+    const handleBooking = (id) =>{
+    //    console.log('ID: ', id)
+    alert(`{ID: ${id}}`)
+    }
     return (
         <div className="card w-[350px] h-[650px] bg-blue-950 mx-auto">
             <figure><img className='h-72 w-full rounded-b-[60px] border-b-4 border-orange-500' src={image} alt="room image"/></figure>
@@ -19,7 +24,7 @@ const Room = ({room}) => {
                 <h2  className="text-xl font-extralight">Price(Per Night): <span className='text-2xl font-medium'>${price-1000}</span></h2>
                 <h3 className='truncate-3-lines text-justify'><span className="text-2xl font-bold text-stone-400">Description: </span>  {description}</h3>
                 <div className="card-actions justify-end absolute bottom-2 w-4/5">
-                    <button className="btn btn-ghost rounded-full bg-gradient-to-r from-pink-500 to-yellow-500 border-none w-full hover:from-black hover:to-black text-xl font-medium">Book Now</button>
+                    <button onClick={()=> handleBooking (_id)} className="btn btn-ghost rounded-full bg-gradient-to-r from-pink-500 to-yellow-500 border-none w-full hover:from-black hover:to-black text-xl font-medium">Book Now</button>
                 </div>
             </div>
         </div>
