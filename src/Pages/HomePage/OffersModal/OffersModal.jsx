c0a0f4e888130346f0176788e1b7e78e746cd0cd
saiful797 <saifulislamsaif797@gmail.com>
2024-05-13 23:13:54 +0600

    Updated offers modal section

--- src/Pages/HomePage/OffersModal/OffersModal.jsx
+++ src/Pages/HomePage/OffersModal/OffersModal.jsx
@@ -3,12 +3,12 @@ import { RxCrossCircled } from 'react-icons/rx';
 
 const OffersModal = ({onClose}) => {
     return (
-        <div className="fixed inset-0 flex justify-start items-end bg-opacity-0 z-50">
-            <div className="lg:ml-[150px] bg-white rounded-lg p-4 max-w-md relative">
+        <div className="fixed inset-0 flex  items-center justify-center bg-opacity-0 z-50">
+            <div className="lg:ml-[150px] bg-lime-100 rounded-lg p-4 max-w-md relative">
                 <h2 className="text-2xl font-bold text-stone-700">Today`s offer! <span className='text-3xl text-yellow-500'>Get 20%</span> discount</h2>
-                <p className='text-xl font-mono'>Booking Your Room Now!</p>
-                <img className='w-38' src="https://i.postimg.cc/bJ02g0xj/17.jpg" alt="" />
-                <button onClick={onClose} className="text-black font-bold py-4 px-4 rounded-full absolute top-0 right-0">
+                <p className='text-xl font-mono text-center mb-2 mt-2'>Booking Your Room Now!</p>
+                <img className='w-38 rounded' src="https://i.postimg.cc/bJ02g0xj/17.jpg" alt="" />
+                <button onClick={onClose} className="text-black text-2xl font-bold py-2 px-2 rounded-full absolute top-0 right-0">
                     <RxCrossCircled />
                 </button>
             </div>
